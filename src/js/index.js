77c4a24e63a4497fcdc7156706364d981af21d9e
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-05-21 13:34:06 +0300

    Add working Seconds hand

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,22 @@
 // jshint esversion: 6
 
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+
+
 function setDate() {
-  console.log("hi");
+  const now = new Date();
+  const seconds = now.getSeconds(); // Gets the seconds of the current minute
+  const secondsDegrees = (seconds / 60) * 360 + 90; // corrects the 90 degree offest
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (seonds / 60) * 360 + 90;
+
+  minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  console.log(seconds);
 }
 
 setInterval(setDate, 1000);
